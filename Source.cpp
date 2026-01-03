dc815a05760b94206404b0c66568299a981b7278
ubejdhetemi <122904251+ubejdhetemi@users.noreply.github.com>
2026-01-03 11:06:26 +0100

    Code for the grading
    Here are the grading functions which based on your points on the exams the grade will be dislpayed

--- Source.cpp
+++ Source.cpp
@@ -15,3 +15,56 @@ struct Subject {
     double grade;                             // 5.0 - 10.0
     string evaluation;                        // Excellent, Very Good, etc.
 };
+double total_percent = 0.0;
+int weighted_sum = 0;
+
+for (int i = 0; i < s.num_components; ++i) {
+    cout << "  Vlermsimi " << (i + 1) << " pershkrim (e.g., Kollokfiumi 1, Projekt): ";
+    getline(cin, s.comp_description[i]);
+
+    cout << "  Perqindja per noten finale " << s.comp_description[i] << " (%): ";
+    cin >> s.comp_percentage[i];
+    while (s.comp_percentage[i] < 0 || s.comp_percentage[i] > 100) {
+        cout << "  Peqindje e gabuar shkruane prap!: ";
+        cin >> s.comp_percentage[i];
+    }
+
+    cout << "  Piket e marrur " << s.comp_description[i] << " (out of 100): ";
+    cin >> s.comp_earned[i];
+    while (s.comp_earned[i] < 0 || s.comp_earned[i] > 100) {
+        cout << "  Pike jo valide shenoni prap (0-100): ";
+        cin >> s.comp_earned[i];
+    }
+
+    weighted_sum += s.comp_earned[i] * s.comp_percentage[i];
+    total_percent += s.comp_percentage[i];
+
+    cin.ignore(); // clear newline after last input
+}
+
+// Calculate final points out of 100
+if (total_percent > 0) {
+    s.total_points = static_cast<int>(weighted_sum / total_percent);
+    if (s.total_points > 100) s.total_points = 100;
+}
+else {
+    s.total_points = 0;
+}
+
+if (total_percent != 100.0) {
+    cout << "  Shenim: piket jane : " << total_percent
+        << "%, Piket finale jane rregulluar.\n";
+}
+}
+
+// Function 4: Input full subject data
+void inputSubject(Subject& s, int number) {
+    cout << "\nLenda " << number << ":" << endl;
+
+    cout << " Emri i Lendes: ";
+    getline(cin, s.name);
+
+    inputEvaluationComponents(s);
+
+    cin.ignore(); // safety for next subject
+}
